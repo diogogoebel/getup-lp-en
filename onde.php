efc4fc6a4a7a600c449f8e20e5a5f43e9ad7897b
diogogoebel <diogo.goebel@gmail.com>
2016-06-10 00:19:45 -0300

    ajuste onde.php

--- onde.php
+++ onde.php
@@ -1,5 +1,5 @@
 <?php
-echo "SIGO FIRME E FORTE \o/ "\n";
-echo IP: " . getenv("SERVER_ADDR") . "\n"; 
+echo "SIGO FIRME E FORTE \o/ ;
+// echo IP: " . getenv("SERVER_ADDR") . "\n"; 
 // echo "Estou no POD de HOSTNAME: " . getenv("HOSTNAME") . "\n";
 ?>

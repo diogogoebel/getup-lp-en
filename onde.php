f0c36fbbc064c2880d12fce015d4083cbd019190
diogogoebel <diogo.goebel@gmail.com>
2016-06-10 00:34:32 -0300

    com aspas

--- onde.php
+++ onde.php
@@ -1,4 +1,4 @@
 <?php
-echo FIRME E FORTE \o/ - vivendo no IP: " . getenv("SERVER_ADDR") . "\n"; 
+echo " ESTOU FIRME E FORTE \o/ - vivendo no IP: " . getenv("SERVER_ADDR") . "\n"; 
 // echo "Estou no POD de HOSTNAME: " . getenv("HOSTNAME") . "\n";
 ?>

750712cb4545f0da34643849adc870ce342e5eb1
diogogoebel <diogo.goebel@gmail.com>
2016-06-10 00:16:58 -0300

    nova mensagem

--- onde.php
+++ onde.php
@@ -1,5 +1,5 @@
 <?php
-echo "Estou firme e forte \o/
+echo "SIGO FIRME E FORTE \o/ "\n";
 echo IP: " . getenv("SERVER_ADDR") . "\n"; 
 // echo "Estou no POD de HOSTNAME: " . getenv("HOSTNAME") . "\n";
 ?>

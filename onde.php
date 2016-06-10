4fdeda631b9a822fd7dd59423315b31fc4313f26
diogogoebel <diogo.goebel@gmail.com>
2016-06-10 00:27:09 -0300

    onde.php

--- onde.php
+++ onde.php
@@ -1,5 +1,5 @@
 <?php
-echo SIGO FIRME E FORTE \o/ 
+echo SIGO FIRME E FORTE \o/  . "/n"; 
 // echo IP: " . getenv("SERVER_ADDR") . "\n"; 
 // echo "Estou no POD de HOSTNAME: " . getenv("HOSTNAME") . "\n";
 ?>

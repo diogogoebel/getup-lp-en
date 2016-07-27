c8ebc56744eadbcd8f8841c2a6a84e793fd7fe13
diogogoebel <diogo.goebel@gmail.com>
2016-07-27 10:29:42 -0300

    mensagem do IP

--- onde.php
+++ onde.php
@@ -1,4 +1,4 @@
 <?php
-echo " ESTOU FIRME E FORTE \o/ - vivendo no IP: " . getenv("SERVER_ADDR") . "\n"; 
+echo " ESTOU NO IP: " . getenv("SERVER_ADDR") . "\n"; 
 // echo "Estou no POD de HOSTNAME: " . getenv("HOSTNAME") . "\n";
 ?>
